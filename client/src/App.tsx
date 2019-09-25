5e4ff603483e3e4df0ce7b514c9b208ebb9b0682
Thibaud REY <contact@thibaudrey.pro>
2019-09-25 23:33:19 +0200

    Full duplex connections exchange, closes #9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
+import 'reflect-metadata';
 import React    from 'react';
 import './App.css';
-import HomePage from './HomePage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from './Components/HomePage';
+import Meeting  from './Components/Meeting';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+          <Switch>
+              <Route exact path='/'>
+                  <HomePage/>
+              </Route>
+              <Route path='/:roomId' component={Meeting}/>
+          </Switch>
+      </Router>
     </div>
   );
 }
