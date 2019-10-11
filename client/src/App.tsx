4690ee2b02f6070fd8a2c6dfa1bf7bd34e4dcefb
AnthonyPierrat <apierrat24@gmail.com>
2019-10-11 14:07:21 +0200

    landing page #12

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
 import 'reflect-metadata';
-import React    from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './Components/HomePage';
-import Meeting  from './Components/Meeting';
+import Meeting from './Components/Meeting';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div>
       <Router>
-          <Switch>
-              <Route exact path='/'>
-                  <HomePage/>
-              </Route>
-              <Route path='/:roomId' component={Meeting}/>
-          </Switch>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/:roomId' component={Meeting} />
+        </Switch>
       </Router>
     </div>
   );
--- client/src/Components/HomePage/Button/index.tsx
+++ client/src/Components/HomePage/Button/index.tsx
@@ -11,14 +11,9 @@ export default class Button extends Component<ButtonProps, {}> {
     render() {
 
         return (
-            <div id="container">
-                <button className="learn-more" onClick={() => {if (this.props.onClick) this.props.onClick()}}>
-                    <div className="circle">
-                        <span className="icon arrow"/>
-                    </div>
-                    <p className="button-text">{this.props.text}</p>
-                </button>
-            </div>
+            <button type="button" className="btn btn-primary" onClick={() => { if (this.props.onClick) this.props.onClick() }}>
+                {this.props.text}
+            </button>
         );
     }
 }
